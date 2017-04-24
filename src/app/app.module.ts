75a92ecc7bb0b913c01013f56d07910ed13c27eb
Angulartraining2016 <AngularTraining2016@github.com>
2017-04-24 09:01:58 +0530

    routing is complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent }  from './app.component';
 import { HeaderComponent }  from './header/header.component';
 import { RecipesComponent }  from './recipes/recipes.component';
@@ -9,9 +9,22 @@ import { RecipesDetailComponent }  from './recipes/recipes-detail/recipes-detail
 import { RecipesItemComponent }  from './recipes/recipes-list/recipes-item/recipes-item.component';
 import { ShoppingListComponent }  from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent }  from './shopping-list/shopping-edit/shopping-edit.component';
+
+const appRoutes: Routes = [
+{path:'recipes',component:RecipesComponent},
+{path:'shopping-list',component:ShoppingListComponent}
+
+]
+//@NgModule takes a metadata object that tells Angular how to compile and run module code.
+//
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ 
+  imports:      [ BrowserModule,RouterModule.forRoot(appRoutes,{useHash:true})],//import module name
+  //The imports array--  When we required router and http feature then we have to add RouterModule and HttpModule inside the import array
+  //Add a module to the imports array when the application requires its features.
+  //This application, like most applications, executes in a browser. Every application that executes in a browser needs the BrowserModule from @angular/platform-browser. So every such application includes the BrowserModule in its root AppModule's imports array.
+  // includes all module which are required to use feature of that module.
+  
+  declarations: [  // list of all component which is required
   AppComponent,
   HeaderComponent,
   RecipesComponent,
@@ -21,6 +34,16 @@ import { ShoppingEditComponent }  from './shopping-list/shopping-edit/shopping-e
   ShoppingListComponent,
   ShoppingEditComponent
   ],
-  bootstrap:    [ AppComponent ]
-})
+  //The declarations array-- You tell Angular which components belong to the AppModule by listing it in the module's declarations array. As you create more components, you'll add them to declarations.
+  //You must declare every component in an NgModule class. If you use a component without declaring it, you'll see a clear error message in the browser console.
+  // so you needs to declare all component which are we create.Inside the NgModule.
+  
+  bootstrap:[ AppComponent ] 
+  // AppComponent is a root component and is added in index.html in case of bootstraping process
+  //the root component that Angular creates and inserts into the index.html
+//The bootstrap array --- You launch the application by bootstrapping the root AppModule. 
+ // means in case of bootstrapping process 'AppComponent' attach into index.html
+
+ })
+
 export class AppModule { }
--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -21,8 +21,19 @@ export class RecipesListComponent{
             name:'Recipes3',
             description:'Description3',
 			image:'https://upload.wikimedia.org/wikipedia/commons/e/e2/Brunswick_stew.jpg'
+        },
+		{
+            name:'Recipes3',
+            description:'Description3',
+			image:'https://upload.wikimedia.org/wikipedia/commons/e/e2/Brunswick_stew.jpg'
+        },
+		{
+            name:'Recipes3',
+            description:'Description3',
+			image:'https://upload.wikimedia.org/wikipedia/commons/e/e2/Brunswick_stew.jpg'
         }
         ];
   }
-  
+   value = '';
+  update(value: string) { this.value = value; }
  }
